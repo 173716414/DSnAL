93818564ac3f27132fb35941e7ebee97946da384
Victor <47047497+173716414@users.noreply.github.com>
2024-03-05 11:17:00 +0800

    设计模式-外观模式-day6

---
+++ src/main/java/designPattern/facade/Main.java
@@ -0,0 +1,52 @@
+package designPattern.facade;
+
+// ⼦系统A
+class SubsystemA {
+    public void operationA() {
+        System.out.println("SubsystemA operation");
+    }
+}
+
+// ⼦系统B
+class SubsystemB {
+    public void operationB() {
+        System.out.println("SubsystemB operation");
+    }
+}
+
+// ⼦系统C
+class SubsystemC {
+    public void operationC() {
+        System.out.println("SubsystemC operation");
+    }
+}
+
+// 外观类
+class Facade {
+    private SubsystemA subsystemA;
+    private SubsystemB subsystemB;
+    private SubsystemC subsystemC;
+
+    public Facade() {
+        this.subsystemA = new SubsystemA();
+        this.subsystemB = new SubsystemB();
+        this.subsystemC = new SubsystemC();
+    }
+
+    // 外观⽅法，封装了对⼦系统的操作
+    public void facadeOperation() {
+        subsystemA.operationA();
+        subsystemB.operationB();
+        subsystemC.operationC();
+    }
+}
+
+// 客户端
+public class Main {
+    public static void main(String[] args) {
+// 创建外观对象
+        Facade facade = new Facade();
+// 客户端通过外观类调⽤⼦系统的操作
+        facade.facadeOperation();
+    }
+}
