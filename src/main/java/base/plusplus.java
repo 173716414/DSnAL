b9dab032170cb13476e861e5eae42c5ee4a35bdd
Victor <47047497+173716414@users.noreply.github.com>
2024-07-23 17:23:37 +0800

    i = i++

---
+++ src/main/java/base/plusplus.java
@@ -0,0 +1,19 @@
+package base;
+
+/*
+ *Author：Victor_htq
+ *Package：base
+ *Project：DSnAL
+ *name：plusplus
+ *Date：2024/7/20  20:37
+ *Filename：plusplus
+ */
+public class plusplus {
+    public static void main(String[] args) {
+        int i = 1;
+        i = i++;
+
+        // System.out.println(a);
+        System.out.println(i);
+    }
+}
